8f3ee908f4f533a5afeec33b0363560d219d7b83
RensSchoonman <100193437+RensSchoonman@users.noreply.github.com>
2023-03-01 12:00:16 +0100

    Add files via upload

---
+++ homepage.js
@@ -0,0 +1,26 @@
+window.addEventListener('load', function() {
+  var introSection = document.getElementById('intro-section');
+  introSection.classList.add('hidden');
+});
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
+
+
